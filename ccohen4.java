b1592debef36ca1fe08ef8759ee5f0cf9dff0275
omega444 <ccohen421@gmail.com>
2025-02-05 02:22:36 +0000

    lab#4 first version

--- ccohen4.java
+++ ccohen4.java
@@ -6,15 +6,16 @@ public class ccohen4
 	public static void main(String [] args)
 	{
 		int x = 0;
-
-		System.out.println("welcome adventurer! Which class would you like to play as?");
 		Scanner input = new Scanner(System.in);
+		System.out.println("welcome adventurer! Which class would you like to play as?");
+	
 		System.out.println("Option 1: Assassin");
 		System.out.println("Option 2: Broke College Student");
 		System.out.println("Option 3: rich college professor(hypothetical)");
 		System.out.println("Option 4: Mage");
-		System.out.println("You chose "+ input);
+		
 		x= input.nextInt();
+		System.out.println("You chose "+ x);
 		input.close();
 	}
 }
